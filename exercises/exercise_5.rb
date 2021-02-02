289abfa706639ae86173f2c3b8c91d62c53f3259
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-02-02 00:28:51 -0800

    Find Sum And AVG  revenue

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+sum_revenue = Store.sum(:annual_revenue)
+avg_revenue = Store.average(:annual_revenue)
+
+puts sum_revenue
+puts avg_revenue
+
+stores_more_than_million = Store.where('annual_revenue > 1000000')
+puts stores_more_than_million.count
